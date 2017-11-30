eae50711ce7f3034512c9763a86ad44896fee40c
Jessica Ambrosio <ambrosio.jessica96@gmail.com>
2017-11-30 11:45:16 -0500

    starting library change

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 This repository contains an implementation of the Chord protocol through HTTP requests.  
 
+Updates:
+	- We'll be using the socketIO library. It allows us to create chat rooms 
+	and to handle messages just as nicely as the qt library. 
+	- Create a virtual environment in a folder in the zoo and install the following:
+		- flask, requests, flask_socketio
+		- This will let us run our app in the zoo. 
+	- To show how CHORD works, we can record the screens of multiple machines in the zoo.
+	  To simplify things, we could also have a form that allows the user to input the ip address and port of a known, stable Chord user that it can connect to. 
+
 Contributors: John Amadeo and Jessica Ambrosio
--- node.py
+++ node.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from flask_socketio import SocketIO
 import sys, math
 import hashlib
 import requests
@@ -16,8 +17,6 @@ def home():
 # Depending on how long the functions become, we could separate this
 # into two files. One containing user functions, and the other one
 # with Chord functions only. 
-
-# Do you want to make a Node object? D:! 
 
 # USER VARIABLES
 nodeID = 0
@@ -43,7 +42,7 @@ def main():
 		sys.exit()
 	render_template("index.html")
 
-# TO DO: MAKE /exist POST function to check if a node alreayd exists. 
+# TO DO: MAKE /exist POST function to check if a node already exists. 
 
 @node.route("/join")
 def join():
@@ -59,29 +58,41 @@ def join():
 		counter = 0
 		idTaken = False
 		(data, address) = nodeSocket.recvfrom(512)  # data -> string
-		# Keep at most 5 nodes available in case the others fail.
+		# Keep at most 5 nodes available in case of disconnects. 
 		while ((not is_empty((data,address))) and (counter < 5)):
-			r = requests.post(str(address) + "/exist", data={'id':nodeID})
+			# The requests can have timeouts. This will help us determine if the 
+			# other node is alive or not. 
+			r = requests.post(str(address) + "/exist", data={'id':nodeID}, timeout=5)
 			if (not is_empty(r)): 
 				neighbors.append((data,address))
+				print r  	   # For debugging purposes. 
 				if r == "YES": # The node already exists.
 					idTaken = True
 			counter += 1
 			(data, address) = nodeSocket.recvfrom(512)
 		if (idTaken):
-			changeID() 	# This function will create a new ID, and  
-						# will send requests to the saved list of neighbors
-						# to check if the ID has been taken. 
+			changeID() 	 
 		else:
 			makeFingers() # Make the finger table! :D 
 	except socket.timeout:
 		print "YOU ARE THE FIRST NODE IN THE CHORD SYSTEM."
 	
-# Make function to check if there exists a node with a given 
-# 
+
+# Returns "YES" if the ID has already been taken
+# and "NO" otherwise. 
+@node.route("/exist", methods["POST"])
+def exist():
+	recID = request.form['id']
+	# Do something to check if the node ID is taken or not.
+	return "NO"  
+
+
 @node.route("/leave")
 def leave():
+	# Let the nodes on your table know that you are leaving
+	# through a POST request. 
 	return "<h1>You have successfully exited chord.</h1>"
+
 
 @node.route("/search") # This will be a GET request. 
 def search():
@@ -126,6 +137,15 @@ def genID():
 	IP = socket.gethostbyname(hostname)
 	ID = hashlib.sha1(IP) % math.pow(2, idBits)
 	return string(ID)
+
+
+# This function will create a new ID, and  
+# will send requests to the saved list of neighbors
+# to check if the ID has been taken. This will be repeated
+# until we determine a valid ID. 
+def changeID():
+	print "I will do something"
+
 
 def between(a, b, c):
 	if b > a:
